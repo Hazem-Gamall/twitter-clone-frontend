ff920533766155d5f4a894bf8dbb40e206dd9a6e
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-19 22:50:52 +0300

    auth context

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme.ts";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript
-          initialColorMode={theme.config.initialColorMode}
-        ></ColorModeScript>
-        <App />
-      </ChakraProvider>
+      <AuthProvider>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript
+            initialColorMode={theme.config.initialColorMode}
+          ></ColorModeScript>
+          <App />
+        </ChakraProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
