900e739db65b0bfd95b27e2ff1c1427adaf7a8e1
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-22 14:33:35 +0300

    localstorage is synced to the auth context

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,16 @@ import { useContext } from "react";
 import AuthContext, { IAuthContext } from "../context/AuthProvider";
 
 const useAuth = () => {
-  return useContext<IAuthContext>(AuthContext);
+  const { auth, setAuth } = useContext<IAuthContext>(AuthContext);
+
+  return {
+    auth,
+    setAuth: (data: any) => {
+      if (Object.keys(data).length === 0) localStorage.removeItem("auth");
+      else localStorage.setItem("auth", JSON.stringify(data));
+      setAuth(data);
+    },
+  };
 };
 
 export default useAuth;
