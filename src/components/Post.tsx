12b60f97c7ac4b8c051f216a327d8bb08dfecaba
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-23 11:18:22 +0300

    switched to grid in Post body

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -10,6 +10,8 @@ import {
   LinkBox,
   LinkOverlay,
   Text,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 import IPost from "./Profile/types/Post";
 import { Link } from "react-router-dom";
@@ -29,40 +31,52 @@ export const Post = ({ post }: Props) => {
         p={2}
         borderRadius={0}
       >
-        <CardHeader py={0} pt={3}>
-          <HStack justifyContent={"space-between"}>
-            <HStack>
-              <Avatar src={post.post_user.avatar} />
-              <ChakraLink as={Link} to={`/${post.post_user.username}`}>
-                <Text fontWeight={"bold"}>{post.post_user.name}</Text>
-              </ChakraLink>
-              <ChakraLink
-                as={Link}
-                to={`/${post.post_user.username}`}
-                fontWeight={"light"}
-                color={"gray.500"}
-              >
-                @{post.post_user.username} &#183; {post.creation}
-              </ChakraLink>
-            </HStack>
-            <Button
-              zIndex={1}
-              variant={"ghost"}
-              borderRadius={30}
-              onClick={() => console.log("button click")}
-            >
-              ...
-            </Button>
-          </HStack>
-        </CardHeader>
+        <CardHeader py={0} pt={3}></CardHeader>
         <LinkOverlay
           as={Link}
           to={`/${post.post_user.username}/status/${post.id}`}
         >
-          <CardBody px={20} py={0} pb={5}>
-            <Text>{post.text}</Text>
+          <CardBody py={0} pb={5}>
+            <Grid
+              templateAreas={`"avatar content"`}
+              templateColumns={"1fr 10fr"}
+            >
+              <GridItem area={"avatar"}>
+                <Avatar
+                  as={Link}
+                  to={`/${post.post_user.username}`}
+                  src={post.post_user.avatar}
+                />
+              </GridItem>
+              <GridItem area={"content"}>
+                <HStack justifyContent={"space-between"}>
+                  <HStack>
+                    <ChakraLink as={Link} to={`/${post.post_user.username}`}>
+                      <Text fontWeight={"bold"}>{post.post_user.name}</Text>
+                    </ChakraLink>
+                    <ChakraLink
+                      as={Link}
+                      to={`/${post.post_user.username}`}
+                      fontWeight={"light"}
+                      color={"gray.500"}
+                    >
+                      @{post.post_user.username} &#183; {post.creation}
+                    </ChakraLink>
+                  </HStack>
+                  <Button
+                    zIndex={1}
+                    variant={"ghost"}
+                    borderRadius={30}
+                    onClick={() => console.log("button click")}
+                  >
+                    ...
+                  </Button>
+                </HStack>
+                <Text>{post.text}</Text>
 
-            <Image></Image>
+                <Image></Image>
+              </GridItem>
+            </Grid>
           </CardBody>
         </LinkOverlay>
       </Card>
