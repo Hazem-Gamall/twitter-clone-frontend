84bfcbb2c95017f47ab4040a57068c27c9f1aa6a
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-23 13:41:30 +0300

    fixed nested anchor tags

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,79 +7,78 @@ import {
   HStack,
   Image,
   Link as ChakraLink,
-  LinkBox,
-  LinkOverlay,
   Text,
   Grid,
   GridItem,
 } from "@chakra-ui/react";
-import IPost from "./Profile/types/Post";
-import { Link } from "react-router-dom";
+import IPost from "../types/Post";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 interface Props {
   post: IPost;
 }
 
 export const Post = ({ post }: Props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
-    <LinkBox as={"article"} width={"100%"} borderBottomWidth={1}>
-      <Card
-        width={"100%"}
-        variant={"unstyled"}
-        bg="black"
-        _hover={{ bg: "gray.900" }}
-        p={2}
-        borderRadius={0}
-      >
-        <CardHeader py={0} pt={3}></CardHeader>
-        <LinkOverlay
-          as={Link}
+    <Card
+      width={"100%"}
+      variant={"unstyled"}
+      bg="black"
+      _hover={{ bg: "gray.900", cursor: "pointer" }}
+      p={2}
+      borderRadius={0}
+      onClick={() =>
+        navigate(`/${post.post_user.username}/status/${post.id}`, {
+          state: { from: location.pathname },
+        })
+      }
+    >
+      <CardHeader py={0} pt={3}></CardHeader>
+
+      {/* as={Link}
           to={`/${post.post_user.username}/status/${post.id}`}
-        >
-          <CardBody py={0} pb={5}>
-            <Grid
-              templateAreas={`"avatar content"`}
-              templateColumns={"1fr 10fr"}
-            >
-              <GridItem area={"avatar"}>
-                <Avatar
+         */}
+      <CardBody py={0} pb={5}>
+        <Grid templateAreas={`"avatar content"`} templateColumns={"1fr 10fr"}>
+          <GridItem area={"avatar"}>
+            <Avatar
+              as={Link}
+              to={`/${post.post_user.username}`}
+              src={post.post_user.avatar}
+            />
+          </GridItem>
+          <GridItem area={"content"}>
+            <HStack justifyContent={"space-between"}>
+              <HStack>
+                <ChakraLink as={Link} to={`/${post.post_user.username}`}>
+                  <Text fontWeight={"bold"}>{post.post_user.name}</Text>
+                </ChakraLink>
+                <ChakraLink
                   as={Link}
                   to={`/${post.post_user.username}`}
-                  src={post.post_user.avatar}
-                />
-              </GridItem>
-              <GridItem area={"content"}>
-                <HStack justifyContent={"space-between"}>
-                  <HStack>
-                    <ChakraLink as={Link} to={`/${post.post_user.username}`}>
-                      <Text fontWeight={"bold"}>{post.post_user.name}</Text>
-                    </ChakraLink>
-                    <ChakraLink
-                      as={Link}
-                      to={`/${post.post_user.username}`}
-                      fontWeight={"light"}
-                      color={"gray.500"}
-                    >
-                      @{post.post_user.username} &#183; {post.creation}
-                    </ChakraLink>
-                  </HStack>
-                  <Button
-                    zIndex={1}
-                    variant={"ghost"}
-                    borderRadius={30}
-                    onClick={() => console.log("button click")}
-                  >
-                    ...
-                  </Button>
-                </HStack>
-                <Text>{post.text}</Text>
+                  fontWeight={"light"}
+                  color={"gray.500"}
+                >
+                  @{post.post_user.username} &#183; {post.creation}
+                </ChakraLink>
+              </HStack>
+              <Button
+                zIndex={1}
+                variant={"ghost"}
+                borderRadius={30}
+                onClick={() => console.log("button click")}
+              >
+                ...
+              </Button>
+            </HStack>
+            <Text>{post.text}</Text>
 
-                <Image></Image>
-              </GridItem>
-            </Grid>
-          </CardBody>
-        </LinkOverlay>
-      </Card>
-    </LinkBox>
+            <Image></Image>
+          </GridItem>
+        </Grid>
+      </CardBody>
+    </Card>
   );
 };
