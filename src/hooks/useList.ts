3feb8b0255ec36bd2092e8606199e126cee2ef08
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-23 17:55:47 +0300

    hooks for api actions

---
+++ src/hooks/useList.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { CanceledError } from "axios";
+import { HttpService } from "../services/HttpService";
+
+const useData = <T>(httpService: HttpService, deps?: [any]) => {
+  const [data, setData] = useState<T[]>(Object);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  useEffect(
+    () => {
+      setIsLoading(true);
+      const { request, cancel } = httpService.list<T>();
+      request
+        .then((resp) => {
+          if (resp) {
+            setData(resp?.data);
+            setIsLoading(false);
+          }
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setError(error.message);
+          setIsLoading(false);
+        });
+      return () => cancel();
+    },
+    deps ? [...deps] : []
+  );
+  return { data, isLoading, error };
+};
+
+export default useData;
---
+++ src/hooks/useRetrieve.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { HttpService } from "../services/HttpService";
+import { CanceledError } from "axios";
+
+const useRetrieve = <T>(httpService: HttpService, id: string, deps?: [any]) => {
+  const [data, setData] = useState<T>(Object);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  useEffect(
+    () => {
+      setIsLoading(true);
+      const { request, cancel } = httpService.retrieve<T>(id);
+      request
+        .then((resp) => {
+          if (resp) {
+            setData(resp?.data);
+            setIsLoading(false);
+          }
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setIsLoading(false);
+          setError(error.message);
+        });
+
+      return () => cancel();
+    },
+    deps ? [...deps] : []
+  );
+
+  return { data, isLoading, error };
+};
+
+export default useRetrieve;
