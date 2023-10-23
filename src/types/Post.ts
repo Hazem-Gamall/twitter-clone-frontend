07421e92e641dcfcabb44f276980ea3567807b3a
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-23 12:29:27 +0300

    added Bio field to IUserProfile

---
+++ src/types/Post.ts
@@ -0,0 +1,19 @@
+export default interface IPost {
+  id: number;
+  media: [
+    {
+      id: number;
+      file: string;
+      post: number;
+    }
+  ];
+  creation: string;
+  text: string;
+  repost: false;
+  post_user: {
+    name: string;
+    avatar: string;
+    username: string;
+  };
+  embed: boolean;
+}
