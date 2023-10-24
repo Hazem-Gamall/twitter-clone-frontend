e27771b74c01233b0564320a3f2b4df3c9a7b5a6
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-10-24 13:28:57 +0300

    back button works

--- src/components/Profile/components/TopBar.tsx
+++ src/components/Profile/components/TopBar.tsx
@@ -1,7 +1,9 @@
 import { HStack, Heading, IconButton, Text, VStack } from "@chakra-ui/react";
 import { IoArrowBackOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 export const TopBar = ({ username }: { username: string | undefined }) => {
+  const navigate = useNavigate();
   return (
     <>
       <HStack justifyContent={"flex-start"} px={5}>
@@ -11,6 +13,7 @@ export const TopBar = ({ username }: { username: string | undefined }) => {
           variant={"ghost"}
           fontSize={"xl"}
           borderRadius={30}
+          onClick={() => navigate(-1)}
         />
         <VStack spacing={0} alignItems={"flex-start"} pl={3}>
           <Heading as="h5" fontSize={"lg"}>
